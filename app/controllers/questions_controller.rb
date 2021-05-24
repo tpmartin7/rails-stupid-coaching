26d947ffe719f22329792ec5304db0dad3215434
Timothy Martin <tpmartin7@gmail.com>
2021-05-24 12:34:29 +0100

    done but the testing

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  def ask; end
+
+  def answer
+    @question = params[:question]
+    return unless @question
+
+    @answer = if @question.downcase == 'i am going to work'
+                'Great!'
+              elsif @question[-1] == '?'
+                'Silly question, get dressed and go to work!'
+              else
+                'I don\'t care, get dressed and go to work!'
+              end
+  end
+end
